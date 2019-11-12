6b3b0467f0c60aa71ce304b6f5a2ff6fb50e9eb9
ista2000 <mishra.istasis@gmail.com>
2019-11-13 05:28:36 +0530

    Completed some functionalities

--- Script.py
+++ Script.py
@@ -1,6 +1,7 @@
 import pymysql
 import pymysql.cursors
 
+
 def newStudent():
     try:
         print("Adding New Student")
@@ -23,6 +24,7 @@ def newStudent():
         print("Error!!")
         print(e)
 
+
 def newSwipeEntry():
     try:
         print("Adding New Swipe entry")
@@ -42,14 +44,17 @@ def newSwipeEntry():
         print("Error!!")
         print(e)
 
+
 def newFoodItem():
     '''
     with ingredients and their quantity
     '''
     pass
 
+
 def newIngredient():
     pass
+
 
 def changeMealPrice():
     try:
@@ -69,6 +74,7 @@ def changeMealPrice():
         print("Error!!")
         print(e)
 
+
 def changeIngredientPrice():
     try:
         print("Changing ingredient price")
@@ -86,39 +92,155 @@ def changeIngredientPrice():
         print("Error!!")
         print(e)
 
-def add_change_registered_mess():
 
+def add_change_registered_mess():
+    try:
+        print("Changing mess registration")
+        mess_id = int(input("Registered Mess ID: "))
+        student_id = int(input("Student ID: "))
+        meal_id = int(input("Meal ID: "))
+        # noinspection SqlNoDataSourceInspection
+        sql = "SELECT * FROM registration WHERE student_id = {} AND meal_id = {};".format(student_id, meal_id)
+        cur = connection.cursor()
+        cur.execute(sql)
+        # connection.commit()
+        if cur.rowcount == 1:
+            # noinspection SqlNoDataSourceInspection
+            sql = "UPDATE registration SET registered_mess_id = {} WHERE student_id = {} AND meal_id = {};".format(mess_id, student_id, meal_id)
+        else:
+            # noinspection SqlNoDataSourceInspection
+            sql = "INSERT INTO registration (`registered_mess_id`, `student_id`, `meal_id`) VALUES ({}, {}, {})".format(mess_id, student_id, meal_id)
+        cur.execute(sql)
+        connection.commit()
+
+    except Exception as e:
+        connection.rollback()
+        print("Error!!")
+        print(e)
     pass
+
 
 def changeEmplyeeSalary():
-    pass
+    try:
+        print("Changing employee salary")
+        emp_id = int(input("Employee ID: "))
+        new_salary = int(input("New Salary: "))
+        # noinspection SqlNoDataSourceInspection
+        sql = "UPDATE employee SET Salary = {} WHERE Employee_id = {};".format(new_salary, emp_id)
+        cur = connection.cursor()
+        cur.execute(sql)
+        connection.commit()
+        print("Successfully updated salary")
+
+    except Exception as e:
+        connection.rollback()
+        print("Error!!")
+        print(e)
+
 
 def deleteFoodItem():
-    pass
+    try:
+        print("Deleting food item")
+        food_id = int(input("Food ID: "))
+        # noinspection SqlNoDataSourceInspection
+        sql = "DELETE FROM food_items WHERE Food_id = {}".format(food_id)
+        cur = connection.cursor()
+        cur.execute(sql)
+        connection.commit()
+        print("Successfully deleted food item")
+
+    except Exception as e:
+        connection.rollback()
+        print("Error!!")
+        print(e)
+
 
 def removeStudent():
-    pass
+    try:
+        print("Deleting student")
+        student_id = int(input("Student ID: "))
+        # noinspection SqlNoDataSourceInspection
+        sql = "DELETE FROM students WHERE roll_no = {}".format(student_id)
+        cur = connection.cursor()
+        cur.execute(sql)
+        connection.commit()
+        print("Successfully deleted student")
+
+    except Exception as e:
+        connection.rollback()
+        print("Error!!")
+        print(e)
+
 
 def requiredIngredients():
     '''
     for all the meals of a day, from all messes, ingredients and quantity required for all their food items
     '''
-    pass
+    try:
+        print("Printing required ingredients for the day")
+        day = input("Day: ")
+        # noinspection SqlNoDataSourceInspection
+        sql = """SELECT ingredients.Name, SUM(food_item_ingredients_requirements.Quantity) AS Qty, 
+            SUM(food_item_ingredients_requirements.Quantity) * ingredients.Cost AS Total_cost 
+            FROM food_item_ingredients_requirements, ingredients 
+            WHERE food_item_ingredients_requirements.Food_item_id IN 
+            (SELECT Food_id FROM food_items WHERE Menu_id IN 
+            (SELECT Menu_id FROM menu WHERE menu.meal_id IN 
+            (SELECT meal_id FROM meal WHERE day='{}'))) 
+            AND ingredients.Ingredients_id = food_item_ingredients_requirements.ingredient_id 
+            GROUP BY ingredient_id;""".format(day)
+        cur = connection.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        desc = cur.description
+
+        print("{0:>20} {1:>20} {2:>20}".format(desc[0][0], desc[1][0], desc[2][0]))
+
+        for row in rows:
+            print("{0:>20} {1:>20} {2:>20}".format(row['Name'], row['Qty'], row['Total_cost']))
+
+    except Exception as e:
+        connection.rollback()
+        print("Error!!")
+        print(e)
+
 
 def messMenu():
     pass
+
 
 def sortMeal():
     '''
     for a mess sort all the meals based on card swipes
     '''
+    try:
+        print("Printing popular meals in a mess")
+        mess = input("Mess ID: ")
+        # noinspection SqlNoDataSourceInspection
+        sql = "SELECT meal.Day AS Day, meal.Time AS Time, COUNT(card_swipes.swipe_id) AS Students FROM meal, card_swipes WHERE card_swipes.Mess_id = {} AND meal.Meal_id = card_swipes.Meal_id GROUP BY meal.meal_id ORDER BY COUNT(card_swipes.swipe_id) DESC;".format(mess)
+        cur = connection.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        desc = cur.description
+
+        print("{0:>20} {1:>20} {2:>20}".format(desc[0][0], desc[1][0], desc[2][0]))
+
+        for row in rows:
+            print("{0:>20} {1:>20} {2:>20}".format(row['Day'], row['Time'], row['Students']))
+
+    except Exception as e:
+        connection.rollback()
+        print("Error!!")
+        print(e)
     pass
+
 
 def generate_and_update_revenue():
     '''
     for a day (today) generate all the revenues and cost and expected revenue
     '''
     pass
+
 
 def registered_student_list():
     '''
